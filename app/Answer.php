d80140d077e751afebdbe870311d21533b3cf9b1
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2018-09-24 15:58:46 +0900

    display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -13,7 +13,7 @@ class Answer extends Model
 
     public function user()
     {
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function getBodyHtmlAttribute()
@@ -29,5 +29,10 @@ class Answer extends Model
             $answer->question->increment('answers_count');
             $answer->question->save();
         });
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
