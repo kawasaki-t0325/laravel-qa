972195cd765626c2d5b075e1ec0b53ffd81a5efe
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2018-10-07 12:29:20 +0900

    Create vue answer component

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -51,7 +51,14 @@ class AnswersController extends Controller
             'body' => 'required',
         ]));
 
-        return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
+        if ($request->expectsJson()) {
+            return response()->json([
+                'message' => 'Your answer has been updated',
+                'body_html' => $answer->body_html
+            ]);
+        }
+
+        // return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
     }
 
     /**
